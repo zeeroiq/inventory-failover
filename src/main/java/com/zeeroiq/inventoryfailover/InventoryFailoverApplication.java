f69e80614faca7dbc8826a79477acb42a60ff3d7
zeero1q <shrikantvishwakrma74@gmail.com>
2020-10-26 20:21:04 +0530

    project initialization files. closes #1

---
+++ src/main/java/com/zeeroiq/inventoryfailover/InventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.zeeroiq.inventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryFailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryFailoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeeroiq/inventoryfailover/InventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeroiq.inventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
